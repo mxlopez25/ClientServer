19c2a00a897f9f94bf2cb65f7b5f43cd71be6d30
Maximo Lopez <maximo.lopez.dev@gmail.com>
2021-09-29 11:28:08 -0400

    UserService methods implemented

--- client_server/Services/UserService.cs
+++ client_server/Services/UserService.cs
@@ -2,24 +2,31 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using client_server.Models;
 using client_server.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace client_server.Services
 {
     public class UserService : IUserService
     {
-        public UserService()
-        {
+        private readonly DataContext _dataContext;
 
+        public UserService(DataContext dataContext)
+        {
+            _dataContext = dataContext;
         }
 
         public async Task<List<User>> All()
         {
-            throw new System.NotImplementedException();
+            var users = await _dataContext.Users.ToListAsync();
+            return users;
         }
 
         public async Task<User> Create(User user)
         {
-            throw new System.NotImplementedException();
+            user.CreatedDate = System.DateTime.Now;
+            _dataContext.Users.Add(user);
+            await _dataContext.SaveChangesAsync();
+            return user;
         }
 
         public async Task<bool> Delete(int Id)
@@ -29,7 +36,8 @@ namespace client_server.Services
 
         public async Task<User> Get(int Id)
         {
-            throw new System.NotImplementedException();
+            var user = await _dataContext.Users.FindAsync(Id);
+            return user;
         }
 
         public async Task<User> Update(int Id, User user)
