9827b19a946e069e5812f64a66da92347027c20f
Maximo Lopez <maximo.lopez.dev@gmail.com>
2021-09-29 09:46:37 -0400

    Updated DataContext

--- client_server/Models/DataContext.cs
+++ client_server/Models/DataContext.cs
@@ -5,20 +5,9 @@ namespace client_server.Models
 {
     public class DataContext : DbContext
     {
-                public DbSet<User> Users { get; set; }
+        public DataContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
         public DbSet<Address> Addresses { get; set; }
-        public string DbPath { get; private set; }
-
-        public DataContext() {
-            var folder = Environment.SpecialFolder.LocalApplicationData;
-            var path = Environment.GetFolderPath(folder);
-            DbPath = $"{path}{System.IO.Path.DirectorySeparatorChar}blogging.db";
-        }
-
-        // The following configures EF to create a Sqlite database file in the
-        // special "local" folder for your platform.
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite($"Data Source={DbPath}");
-
     }
 }
