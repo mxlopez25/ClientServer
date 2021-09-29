5141e60a737295058640f297c5ed527054345afa
Maximo Lopez <maximo.lopez.dev@gmail.com>
2021-09-29 12:19:18 -0400

    Entities added

---
+++ client_server/Entities/EAddress.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace client_server.Entities
+{
+    public class EAddress
+    {
+        public int AddressId { get; set; }
+        public string Street1 { get; set; }
+        public string Street2 { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public string Phone { get; set; }
+        public EUser User { get; set; }
+    }
+}
---
+++ client_server/Entities/EUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace client_server.Entities
+{
+    public class EUser
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<EAddress> Addresses { get; set; }
+    }
+}
