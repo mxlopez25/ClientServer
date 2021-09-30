447f6244e095b6527933d221f90258afa010c9ca
Maximo Lopez <maximo.lopez.dev@gmail.com>
2021-09-30 09:25:10 -0400

    User field changed to UserId

--- client_server/Entities/EAddress.cs
+++ client_server/Entities/EAddress.cs
@@ -4,11 +4,11 @@ namespace client_server.Entities
     public class EAddress
     {
         public int AddressId { get; set; }
+        public int UserId { get; set; }
         public string Street1 { get; set; }
         public string Street2 { get; set; }
         public string City { get; set; }
         public string ZipCode { get; set; }
         public string Phone { get; set; }
-        public EUser User { get; set; }
     }
 }
