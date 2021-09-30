6e3e913bcf15cab35baced1c1187f9c1356cef61
Maximo Lopez <maximo.lopez.dev@gmail.com>
2021-09-30 09:32:20 -0400

    User and Address model added

---
+++ ClientApp/app/src/main/java/com/mlopez/clientapp/AddressModel.java
@@ -0,0 +1,67 @@
+package com.mlopez.clientapp;
+
+public class AddressModel {
+    int addressId;
+    int userId;
+    String street1;
+    String street2;
+    String city;
+    String zipCode;
+    String phone;
+
+    public int getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getStreet1() {
+        return street1;
+    }
+
+    public void setStreet1(String street1) {
+        this.street1 = street1;
+    }
+
+    public String getStreet2() {
+        return street2;
+    }
+
+    public void setStreet2(String street2) {
+        this.street2 = street2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ ClientApp/app/src/main/java/com/mlopez/clientapp/UserModel.java
@@ -0,0 +1,42 @@
+package com.mlopez.clientapp;
+
+import java.util.List;
+
+public class UserModel {
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<String> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<String> addresses) {
+        this.addresses = addresses;
+    }
+
+    int userId;
+    String firstName;
+    String lastName;
+    List<String> addresses;
+}
