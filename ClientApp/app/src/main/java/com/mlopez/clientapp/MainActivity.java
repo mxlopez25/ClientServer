b135e563420adaa43dbd06ec2a0abd22d7ed018a
Maximo Lopez <maximo.lopez.dev@gmail.com>
2021-09-30 09:47:48 -0400

    Basic HTTP calls added to MainActivity

--- ClientApp/app/src/main/java/com/mlopez/clientapp/MainActivity.java
+++ ClientApp/app/src/main/java/com/mlopez/clientapp/MainActivity.java
@@ -3,12 +3,65 @@ package com.mlopez.clientapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txtResult;
+    ListView lvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtResult = findViewById(R.id.txt_result);
+        lvMain = findViewById(R.id.lv_main_list);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://clientserver.azurewebsites.net/api/User/all";
+
+        // Request a string response from the provided URL.
+        StringRequest request = new StringRequest(Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray array = new JSONArray(response);
+                            txtResult.setText("Response: " + response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            txtResult.setText("Error: " + response);
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                txtResult.setText("Error: " + error);
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(request);
     }
 }
