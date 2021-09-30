7dd70fc1f86017c9f68d91ebe4f9053d626773c2
Maximo Lopez <maximo.lopez.dev@gmail.com>
2021-09-30 12:28:47 -0400

    DetailsActivity Added

---
+++ ClientApp/app/src/main/java/com/mlopez/clientapp/DetailsActivity.java
@@ -0,0 +1,96 @@
+package com.mlopez.clientapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.location.Address;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DetailsActivity extends AppCompatActivity {
+    public final static String TAG = "DetailsActivity";
+    public String userId = "0";
+    public String firstName = "";
+    public String lastName = "";
+    ListView lvAddresses;
+    TextView tvUserName;
+    ArrayList<AddressModel> addressList = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        lvAddresses = (ListView) findViewById(R.id.lv_addresses);
+        tvUserName = (TextView) findViewById(R.id.txt_user_name);
+        Bundle extras = getIntent().getExtras();
+        userId = extras.getString("userId");
+        firstName = extras.getString("firstName");
+        lastName = extras.getString("lastName");
+        tvUserName.setText(new StringBuilder()
+        .append(firstName)
+        .append(" ")
+        .append(lastName));
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://clientserver.azurewebsites.net/api/Address/user/1";
+
+        // Request a string response from the provided URL.
+        StringRequest request = new StringRequest(Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray array = new JSONArray(response);
+                            for(int i = 0 ; i < array.length() ; i++) {
+                                JSONObject address = array.getJSONObject(i);
+                                Log.d(TAG, address.toString());
+                                addressList.add(new AddressModel(
+                                        address.getInt("addressId"),
+                                        address.getInt("userId"),
+                                        address.getString("street1"),
+                                        address.getString("street2"),
+                                        address.getString("city"),
+                                        address.getString("zipCode"),
+                                        address.getString("phone")
+                                ));
+                            }
+                            AddressAdapter adapter = new AddressAdapter(getApplicationContext(), addressList);
+                            lvAddresses.setAdapter(adapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+//                            txtResult.setText("Error: " + response);
+                            Log.e(TAG, "Error: " + response);
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Error: " + error.getMessage());
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(request);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        this.finish();
+    }
+}
